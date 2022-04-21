3b17dc10d29fe4f738c1210a86e9993a3415d525
Kiera Bacon <KieraJBacon@Outlook.com>
2022-04-20 23:06:54 -0400

    Added player character rotation. It's adorable!

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -10,8 +10,11 @@ public class PlayerCharacter : MonoBehaviour
     private float slowPercent = 0.5f;
     [SerializeField, Min(0)]
     private float speed = 1;
+    [SerializeField, Min(0)]
+    private float rotationSpeed = 1;
 
     [Header("Interaction")]
+    private bool moveInputProvided = false;
     private Vector2 moveInput = Vector2.zero;
 
     [Header("Component References")]
@@ -25,6 +28,8 @@ public class PlayerCharacter : MonoBehaviour
     private void FixedUpdate()
     {
         rigidbody.velocity = new Vector3(moveInput.x * speed, rigidbody.velocity.y, moveInput.y * speed);
+        if (moveInput != Vector2.zero)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(new Vector3(moveInput.x, 0, moveInput.y)), rotationSpeed * Time.fixedDeltaTime);
     }
 
     public void Move(Vector2 moveInput)
